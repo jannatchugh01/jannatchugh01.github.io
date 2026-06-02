40f6132680d4e4782d1713014e841d5176975110
Jannat <chughjannat72@gmail.com>
2026-06-02 18:04:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("Script Loaded");
+document.addEventListener("DOMContentLoaded", function () {
+
+```
+const buttons = document.querySelectorAll(".filters button");
+const projects = document.querySelectorAll(".project-card");
+
+buttons.forEach(function(button){
+
+    button.addEventListener("click", function(){
+
+        buttons.forEach(function(btn){
+            btn.classList.remove("active");
+        });
+
+        button.classList.add("active");
+
+        const filter = button.textContent.trim().toLowerCase();
+
+        projects.forEach(function(project){
+
+            if(filter === "all"){
+                project.style.display = "flex";
+            }
+            else if(project.classList.contains(filter)){
+                project.style.display = "flex";
+            }
+            else{
+                project.style.display = "none";
+            }
+
+        });
+
+    });
+
+});
+```
+
+});
+
