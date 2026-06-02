b53ad4d8d8a4a33692e5aea7eda738baacc5e949
Jannat <chughjannat72@gmail.com>
2026-06-02 17:04:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const buttons = document.querySelectorAll(".filters button");
+const projects = document.querySelectorAll(".project-card");
+
+buttons.forEach(button => {
+
+```
+button.addEventListener("click", () => {
+
+    const filter = button.textContent.toLowerCase();
+
+    projects.forEach(project => {
+
+        if(filter === "all"){
+            project.style.display = "flex";
+        }
+        else if(project.classList.contains(filter)){
+            project.style.display = "flex";
+        }
+        else{
+            project.style.display = "none";
+        }
+
+    });
+
+});
+```
+
+});
 
